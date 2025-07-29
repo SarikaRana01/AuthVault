849c50aeec2bcac00f7fe71b2ab650b3dfb4ea3f
Sarika06-byte <sarikarana781@gmail.com>
2025-07-29 18:38:33 +0530

    DONE

--- AuthVault/settings.py
+++ AuthVault/settings.py
@@ -100,20 +100,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/kolKata'
+TIME_ZONE = 'asia/kolKata'
 
 USE_I18N = True
 
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-# STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
