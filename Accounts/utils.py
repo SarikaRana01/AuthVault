2389da3da432c4d0299f955eb48ba1855f4651e6
Sarika06-byte <sarikarana781@gmail.com>
2025-07-30 08:29:42 +0530

    done

--- Accounts/utils.py
+++ Accounts/utils.py
@@ -1,4 +1,4 @@
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet,InvalidToken
 from django.conf import settings
 
 fernet = Fernet(settings.ENCRYPTION_KEY.encode())
@@ -6,5 +6,11 @@ fernet = Fernet(settings.ENCRYPTION_KEY.encode())
 def encrypt_password(password):
     return fernet.encrypt(password.encode()).decode()
 
+
+
 def decrypt_password(encrypted_password):
-    return fernet.decrypt(encrypted_password.encode()).decode()
+    try:
+        return fernet.decrypt(encrypted_password.encode()).decode()
+    except (InvalidToken, AttributeError) as e:
+        print(f"[Decryption Error] Problem with: {encrypted_password}")
+        return "[Decryption Failed]"
